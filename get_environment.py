aee3cfc8ecf6b0db39682a55d9a43a280ff6f310
txst54 <33455116+txst54@users.noreply.github.com>
2020-07-12 21:40:56 -0500

    Modified for simultaneous retrieving and training

--- get_environment.py
+++ get_environment.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 import pyautogui
 from time import sleep
-from matplotlib import pyplot as plt
 
 
 class GetEnv:
@@ -11,7 +10,6 @@ class GetEnv:
         self.outDim = outDim
 
     def takeImage(self, num, processing):
-        out = []
         Freq = 28 / num
         step = 0
         for i in range(28):
@@ -22,27 +20,11 @@ class GetEnv:
                                      cv2.COLOR_RGB2GRAY)
                 dim = self.outDim
                 inDim = self.inDim
-                print(inDim[0], inDim[1], inDim[2], inDim[3])
+                # print(inDim[0], inDim[1], inDim[2], inDim[3])
                 img = cv2.resize(image[inDim[0]:inDim[1], inDim[2]:inDim[3]], dim, interpolation=cv2.INTER_AREA)
                 if processing == "inverse":
                     img = cv2.bitwise_not(img)
-                out.append(img)
-                print(img.shape)
+                return img
             sleep(1 / 28)
-        print(len(out))
-        return out
 
-    def step(self, action):
-        if action == 1:
-            pyautogui.press('left')
-        elif action == 2:
-            pyautogui.press('up')
-        elif action == 3:
-            pyautogui.press('down')
-        elif action == 4:
-            pyautogui.press('right')
-        elif action == 5:
-            pyautogui.press('enter')
-        else:
-            sleep(1 / 28)
 
