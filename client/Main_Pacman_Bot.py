ba0cf5aea87700cc9a6223f594dc0a7b9de3acbc
txst54 <33455116+txst54@users.noreply.github.com>
2020-07-14 22:56:19 -0500

    Bug Fixes

--- client/Main_Pacman_Bot.py
+++ client/Main_Pacman_Bot.py
@@ -1,8 +1,9 @@
 from DDQN_Model import ddqnTrainer
 from get_environment import GetEnv
 from gather_training_points import readReward
-import pyautogui
+import pydirectinput
 from time import sleep
+import threading
 
 
 Frames = 4
@@ -10,7 +11,10 @@ OutFrameSize = 80
 InFrameDim = (556, 838, 449, 786)
 InputShape = (Frames, InFrameDim[1]-InFrameDim[0], InFrameDim[3] - InFrameDim[2])
 TotalStepLimit = 5000000
-ActionSpace = ("w", "a", "s", "d", "space", "e", "q")
+ActionSpace = ("w", "a", "s", "d", "space")
+MouseActionSpace = [[868, 360], [860, 398], [839, 431], [806, 452], [768, 460], [729, 452], [697, 430], [675, 398], [668, 360], [675, 321], [697, 289], [729, 267], [768, 260], [806, 267], [838, 289], [860, 321]]
+ScoreInDim = (905, 921, 875, 1045)
+ScoreOutDim = (170, 16)
 
 
 class Environment:
@@ -19,6 +23,11 @@ class Environment:
         self.out = []
         for i in range(5):
             self.out.append(self.env.takeImage(4, "None"))
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True
+        thread.start()
+
+    def run(self):
         while True:
             self.out.append(self.env.takeImage(4, "None"))
             self.out.pop(0)
@@ -26,30 +35,35 @@ class Environment:
     def getEnvironment(self):
         return self.out
 
-    def step(self, action):
+    def step(self, action, prev_action):
         print(f"action: {action}")
-        pyautogui.press(ActionSpace[action])
+        pydirectinput.keyUp(ActionSpace[prev_action[0]])
+        pydirectinput.keyDown(ActionSpace[action[0]])
+        pydirectinput.moveTo(MouseActionSpace[action[1]][0], MouseActionSpace[action[1]][1])
 
 
 def main():
     run = 0
     total_step = 0
-    game_model = ddqnTrainer(InputShape, len(ActionSpace))
+    game_model = ddqnTrainer(InputShape, (len(ActionSpace), len(MouseActionSpace)))
     prev_score = 0
+    prev_action = [0, 0]
     while True:
         run += 1
         env = Environment()
         current_state = env.getEnvironment()
         step = 0
+        print(total_step)
         score = readReward()
         while total_step <= TotalStepLimit:
             total_step += 1
             step += 1
             print(f"Step: {total_step}")
             action = game_model.move(current_state)
-            env.step(action)
+            env.step(action, prev_action)
+            prev_action = action
             next_state = env.getEnvironment()
-            current_score = score.mainLoop()
+            current_score = score.mainLoop(ScoreInDim, ScoreOutDim)
             reward = current_score - prev_score
             print(reward)
             prev_score = current_score
