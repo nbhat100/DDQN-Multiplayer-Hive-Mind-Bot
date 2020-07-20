1dd768aec2277444afbd8069229c9ab127aae30a
dch777 <d.chanana777@gmail.com>
2020-07-20 11:25:20 -0500

    Added server/client code

--- client/client.py
+++ client/client.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
 
 import socket
+import numpy as np
+from io import BytesIO
+from struct import *
+import zlib
 
-host = "localhost"
-port = 8080
+class Client:
+    def __init__(self, host="", port=8080):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((host, port))
+    def send_image_array(self, img):
+        data = zlib.compress(img.tobytes())
+        print(len(data))
+        self.sock.sendall(pack('>Q', len(data)))
+        self.sock.sendall(data)
+        self.sock.recv(1)
+        return unpack('>Q', self.sock.recv(8))
 
-seed = "This is a test of "
-sock.send(seed.encode('ascii'))
-data = sock.recv(1024)
-print(data)
-sock.close()
+client = Client()
+client.send_image_array(np.array([[[.1] * 3] * 1920] * 1080))
