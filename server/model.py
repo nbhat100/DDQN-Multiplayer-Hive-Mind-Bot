da97c0b07c0ed71af3782b59942ad26c813f5021
txst54 <33455116+txst54@users.noreply.github.com>
2020-07-27 19:26:49 -0500

    Update model.py

--- server/model.py
+++ server/model.py
@@ -1,59 +1,67 @@
-#!/usr/bin/python
-
-import numpy as np
-import math
-import tensorflow as tf
-import tensorflow.keras
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import layers, models, optimizers
 from tensorflow.keras.layers import *
 
-class CustomModel(Model):
-    def train_step(self, data):
-        predictionProbability = lambda x, y: math.exp(self.predict(x)) / (math.exp(self.predict(x)) - math.exp(self.predict(y)))
-        compute_loss = lambda data: -1 * sum([mu[0] * math.log(predictedProbability(x,y)) + mu[1] * math.log(predictedProbability(y,x)) for x, y, mu in data])
 
-        with tf.GradientTape() as tape:
-            loss = compute_loss(data)
-        
-        gradients = tape.gradients(loss, model.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+class ConvNet:
+    def __init__(self, input_shape, action_space):
+        inputA = Input(shape=input_shape, name='inputA')
+        inputB = Input(shape=input_shape, name='inputB')
+        inputC = Input(shape=input_shape, name='inputC')
 
-        return {"loss": loss}
+        x = TimeDistributed(Conv2D(32, 8, (4, 4), activation='relu'))(inputA)
+        x = TimeDistributed(MaxPooling2D())(x)
+        x = TimeDistributed(Conv2D(64, 1, (2, 2), activation='relu'))(x)
+        x = TimeDistributed(MaxPooling2D())(x)
+        x = TimeDistributed(Flatten())(x)
+        x = LSTM(128)(x)
+        x = models.Model(inputs=inputA, outputs=x)
 
-def createModel(input_shape):
-    inputA = Input(shape=input_shape, name='inputA')
-    inputB = Input(shape=input_shape, name='inputB')
-    inputC = Input(shape=input_shape, name='inputC')
+        y = TimeDistributed(Conv2D(32, 8, (4, 4), activation='relu'))(inputB)
+        y = TimeDistributed(MaxPooling2D())(y)
+        y = TimeDistributed(Conv2D(64, 1, (2, 2), activation='relu'))(y)
+        y = TimeDistributed(MaxPooling2D())(y)
+        y = TimeDistributed(Flatten())(y)
+        y = LSTM(128)(y)
+        y = models.Model(inputs=inputB, outputs=y)
 
-    x = Conv2D(32, 8, (4, 4), activation='relu')(inputA)
-    x = MaxPooling2D()(x)
-    x = Conv2D(64, 4, (2, 2), activation='relu')(x)
-    x = MaxPooling2D()(x)
-    x = Flatten()(x)
-    x = Dense(256, activation='relu')(x)
-    x = Model(inputs=inputA, outputs=x)
+        z = TimeDistributed(Conv2D(32, 8, (4, 4), activation='relu'))(inputC)
+        z = TimeDistributed(MaxPooling2D())(z)
+        z = TimeDistributed(Conv2D(64, 1, (2, 2), activation='relu'))(z)
+        z = TimeDistributed(MaxPooling2D())(z)
+        z = TimeDistributed(Flatten())(z)
+        z = LSTM(128)(z)
+        z = models.Model(inputs=inputC, outputs=z)
 
-    y = Conv2D(32, 8, (4, 4), activation='relu')(inputB)
-    y = MaxPooling2D()(y)
-    y = Conv2D(64, 4, (2, 2), activation='relu')(y)
-    y = MaxPooling2D()(y)
-    y = Flatten()(y)
-    y = Dense(256, activation='relu')(y)
-    y = Model(inputs=inputB, outputs=y)
+        combined = Concatenate()([x.output, y.output, z.output])
+        combined_input = [x.input, y.input, z.input]
 
-    z = Conv2D(32, 8, (4, 4), activation='relu')(inputC)
-    z = MaxPooling2D()(z)
-    z = Conv2D(64, 4, (2, 2), activation='relu')(z)
-    z = MaxPooling2D()(z)
-    z = Flatten()(z)
-    z = Dense(256, activation='relu')(z)
-    z = Model(inputs=inputC, outputs=z)
+        keya = Dense(action_space[0])(combined)
+        mousea = Dense(action_space[1])(combined)
+        key_a = models.Model(combined_input, keya, name="key_a")
+        mouse_a = models.Model(combined_input, mousea, name="mouse_a")
+        key_a_values = key_a(combined_input)
+        mouse_a_values = mouse_a(combined_input)
 
-    combined = Concatenate()([x.output, y.output, z.output])
-    final = Dense(128, activation='relu')(combined)
-    final = Dense(32, activation='relu')(final)
-    final = Dense(16, activation='relu')(final)
-    final = Dense(1, activation='linear')(final)
-    model = Model(inputs=[x.input, y.input, z.input], outputs=[final])
-    return model
+        keyb = Dense(action_space[0])(combined)
+        mouseb = Dense(action_space[1])(combined)
+        key_b = models.Model(combined_input, keyb, name="key_b")
+        mouse_b = models.Model(combined_input, mouseb, name="mouse_b")
+        key_b_values = key_b(combined_input)
+        mouse_b_values = mouse_b(combined_input)
+
+        keyc = Dense(action_space[0])(combined)
+        mousec = Dense(action_space[1])(combined)
+        key_c = models.Model(combined_input, keyc, name="key_c")
+        mouse_c = models.Model(combined_input, mousec, name="mouse_c")
+        key_c_values = key_c(combined_input)
+        mouse_c_values = mouse_c(combined_input)
+
+        self.model = models.Model(inputs=combined_input, outputs=[[key_a_values, mouse_a_values], [key_b_values, mouse_b_values], [key_c_values, mouse_c_values]], name="full_conv_net")
+        self.model.compile(loss="mean_squared_error",
+                           optimizer=optimizers.RMSprop(lr=0.00025,
+                                                        rho=0.95,
+                                                        epsilon=0.01),
+                           metrics=["accuracy"])
+
+        self.model.summary()
+
